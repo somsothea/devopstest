4f390afe61d83fe522d09d984c5adc0208f2dfbe
tfd-ed <info@tfdevs.com>
2024-11-27 14:10:04 +0700

    Test

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World KKKK 你好');
+    res.status(200).send('Hello World');
 });
 
 app.get('/user/:id', (req, res) => {
-  res.status(200).json({ id: req.params.id });
+    res.status(200).json({ id: req.params.id });
 });
 
 module.exports = app;
