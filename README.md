24f523654758c58513a89160502e70a0663d2508
tfd-ed <info@tfdevs.com>
2024-11-27 13:17:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# CADT DevOps
+
+## Install and run the server
+```sh
+npm install
+npm run dev
+```
+## Run test
+```sh
+npm run test
+```
---
+++ app.test.js
@@ -0,0 +1,20 @@
+// app.test.js
+const request = require('supertest');
+const app = require('./app.js');
+
+// Unit Test
+describe('GET /', () => {
+    it('should return Hello, World!', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toBe('Hello, World KKKK 你好');
+    });
+});
+
+describe('GET /user/:id', () => {
+    it('should return user object with id', async () => {
+        const res = await request(app).get('/user/123');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toHaveProperty('id', '123');
+    });
+});
