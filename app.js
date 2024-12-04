3b906be333c1b69fe299ee7f92abc7fd7ddbbe2b
tfd-ed <info@tfdevs.com>
2024-12-04 14:12:22 +0700

    DevOPs

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World 222');
+    res.status(200).send('Hello World 444');
 });
 
 app.get('/user/:id', (req, res) => {
